a4a3c0134974d79cfaf8e2aeb757271df489b5dd
Khang <kt36@njit.edu>
2022-09-12 11:11:35 -0400

    init

--- feature_create.py
+++ feature_create.py
@@ -26,10 +26,9 @@ def main(args, path_db='./data/feat_store.db'):
     store = FeatureStore(path_db)
     node_type = ['bank', 'account', 'address']
     uid_cols = ['MessageId', 'Timestamp', 'UETR', 'Sender', 'Receiver', 'OrderingAccount', 'BeneficiaryAccount', 'Label', 'OrderingOriginAdd', 'BeneficiaryOriginAdd']
-    if (args.mode == 'train'):
-        df = pd.read_parquet(args.data_path + args.train_feat_path)
-    else:
-        df = pd.read_parquet(args.data_path + args.test_feat_path)
+
+    # Train
+    df = pd.read_parquet(args.data_path + args.train_feat_path)
     feature_cols = list(df.columns)
     for i in uid_cols:
         feature_cols.remove(i)
@@ -43,23 +42,63 @@ def main(args, path_db='./data/feat_store.db'):
     del(temp_df)
     gc.collect()
 
+    # Test
+    df = pd.read_parquet(args.data_path + args.test_feat_path)
+    feature_cols = list(df.columns)
+    for i in uid_cols:
+        feature_cols.remove(i)
+    temp_df = df[['MessageId'] + feature_cols]
+    x = temp_df.values
+    with store.db.write_batch() as wb:
+        for i in range(x.shape[0]):
+            key = 't{}'.format(x[i, 0])
+            value = x[i, 1:]
+            store.put(key, value, wb=wb, dtype=np.float32)
+    del (temp_df)
+    gc.collect()
 
-    node_src = df['src'].drop_duplicates().tolist()
-    node_dst = df['dst'].drop_duplicates().tolist()
-    graph = nx.from_pandas_edgelist(df, source='src', target='dst')
+    # Bank
+    df = pd.read_parquet(args.data_path + args.bank_path)
+    feature_cols = list(df.columns)
+    df['id'] = np.arange(df.shape[0])
+    temp_df = df[['id'] + feature_cols]
+    x = temp_df.values
+    with store.db.write_batch() as wb:
+        for i in range(x.shape[0]):
+            key = 'b{}'.format(x[i, 0])
+            value = x[i, 1:]
+            store.put(key, value, wb=wb, dtype=np.float32)
+    del (temp_df)
+    gc.collect()
 
-    for node_id in tqdm.tqdm(node_dst):
-        sum_feat = None
-        cnt_neighbor = 0
-        for neighbor in graph.neighbors(node_id):
-            if neighbor in node_src:
-                neighbor_feat = store.get(key=neighbor, default_value=None)
-                if sum_feat is None:
-                    sum_feat = neighbor_feat.copy()
-                else:
-                    sum_feat += neighbor_feat
-                cnt_neighbor += 1
-        store.put(key=node_id, value=sum_feat/cnt_neighbor)
+    # Acc
+    df = pd.read_parquet(args.data_path + args.acc_path)
+    feature_cols = list(df.columns)
+    df['id'] = np.arange(df.shape[0])
+    temp_df = df[['id'] + feature_cols]
+    x = temp_df.values
+    with store.db.write_batch() as wb:
+        for i in range(x.shape[0]):
+            key = 'a{}'.format(x[i, 0])
+            value = x[i, 1:]
+            store.put(key, value, wb=wb, dtype=np.float32)
+    del (temp_df)
+    gc.collect()
+
+    # Add
+    df = pd.read_parquet(args.data_path + args.add_path)
+    feature_cols = list(df.columns)
+    df['id'] = np.arange(df.shape[0])
+    temp_df = df[['id'] + feature_cols]
+    x = temp_df.values
+    with store.db.write_batch() as wb:
+        for i in range(x.shape[0]):
+            key = 'ad{}'.format(x[i, 0])
+            value = x[i, 1:]
+            store.put(key, value, wb=wb, dtype=np.float32)
+    del (temp_df)
+    gc.collect()
+
 
 
 if __name__=="__main__":
