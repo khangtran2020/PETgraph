43f16de1eb2f3a4c82ad974b4ba66eab3e7265d9
Khang <kt36@njit.edu>
2022-09-12 11:21:39 -0400

    init

--- feature_create.py
+++ feature_create.py
@@ -35,13 +35,13 @@ def main(args, path_db='./data/feat_store.db'):
     temp_df = df[['MessageId']+feature_cols]
     x = temp_df.values
     with store.db.write_batch() as wb:
-        for i in range(x.shape[0]):
-            key = 'T{}'.format(x[i, 0])
+        for i in tqdm(range(x.shape[0])):
+            key = b'T{}'.format(x[i, 0])
             value = x[i, 1:]
             store.put(key, value, wb=wb, dtype=np.float32)
     del(temp_df)
     gc.collect()
-
+    print("Done for train dataset")
     # Test
     df = pd.read_csv(args.data_path + args.test_feat_path)
     feature_cols = list(df.columns)
@@ -50,12 +50,13 @@ def main(args, path_db='./data/feat_store.db'):
     temp_df = df[['MessageId'] + feature_cols]
     x = temp_df.values
     with store.db.write_batch() as wb:
-        for i in range(x.shape[0]):
-            key = 't{}'.format(x[i, 0])
+        for i in tqdm(range(x.shape[0])):
+            key = b't{}'.format(x[i, 0])
             value = x[i, 1:]
             store.put(key, value, wb=wb, dtype=np.float32)
     del (temp_df)
     gc.collect()
+    print("Done for test dataset")
 
     # Bank
     df = pd.read_csv(args.data_path + args.bank_path)
@@ -64,12 +65,13 @@ def main(args, path_db='./data/feat_store.db'):
     temp_df = df[['id'] + feature_cols]
     x = temp_df.values
     with store.db.write_batch() as wb:
-        for i in range(x.shape[0]):
-            key = 'b{}'.format(x[i, 0])
+        for i in tqdm(range(x.shape[0])):
+            key = b'b{}'.format(x[i, 0])
             value = x[i, 1:]
             store.put(key, value, wb=wb, dtype=np.float32)
     del (temp_df)
     gc.collect()
+    print("Done for bank dataset")
 
     # Acc
     df = pd.read_csv(args.data_path + args.acc_path)
@@ -78,12 +80,13 @@ def main(args, path_db='./data/feat_store.db'):
     temp_df = df[['id'] + feature_cols]
     x = temp_df.values
     with store.db.write_batch() as wb:
-        for i in range(x.shape[0]):
-            key = 'a{}'.format(x[i, 0])
+        for i in tqdm(range(x.shape[0])):
+            key = b'a{}'.format(x[i, 0])
             value = x[i, 1:]
             store.put(key, value, wb=wb, dtype=np.float32)
     del (temp_df)
     gc.collect()
+    print("Done for Acc Dataset")
 
     # Add
     df = pd.read_csv(args.data_path + args.add_path)
@@ -92,15 +95,17 @@ def main(args, path_db='./data/feat_store.db'):
     temp_df = df[['id'] + feature_cols]
     x = temp_df.values
     with store.db.write_batch() as wb:
-        for i in range(x.shape[0]):
-            key = 'ad{}'.format(x[i, 0])
+        for i in tqdm(range(x.shape[0])):
+            key = b'ad{}'.format(x[i, 0])
             value = x[i, 1:]
             store.put(key, value, wb=wb, dtype=np.float32)
     del (temp_df)
     gc.collect()
+    print("Done for Add dataset")
+
 
 
 
 if __name__=="__main__":
     args = parse_args()
-    fire.Fire(main(args=args))
+    main(args=args)
