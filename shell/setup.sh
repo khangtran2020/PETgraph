f80a6010d0c406b9142a60d6b623b91c80a09971
Khang <kt36@njit.edu>
2022-09-22 12:51:44 -0400

    init

---
+++ shell/setup.sh
@@ -0,0 +1,10 @@
+# !/bin/bash
+# run this script under project's dir
+conda install pytorch==1.8.1 torchvision==0.9.1 torchaudio==0.8.1 cudatoolkit=10.1 -c pytorch -y
+conda install ignite -c pytorch -y
+
+pip install --no-index \
+    torch-scatter torch-sparse torch-cluster torch-spline-conv \
+    -f https://pytorch-geometric.com/whl/torch-1.8.1+cu101.html
+
+pip install torch-geometric==1.7.2
