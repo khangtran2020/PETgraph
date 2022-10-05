296f18eb62b654fe37c3c888c107083c0f656cc2
Khang <kt36@njit.edu>
2022-10-05 14:55:38 -0400

    init

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cp
 if __name__ == '__main__':
     args = parse_args()
     args.batch_size = (args.batch_size_0, args.batch_size_1)
-    if args.main_mode == 'central':
+    if args.mode == 'central':
         run_central(args=args)
     else:
         run_para(args)
